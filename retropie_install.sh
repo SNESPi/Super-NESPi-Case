8eca4c770d4b98807f3589482a542306e62d62b9
SNESPi <40364393+SNESPi@users.noreply.github.com>
2018-08-07 16:00:29 +0800

    Update retropie_install.sh

--- retropie_install.sh
+++ retropie_install.sh
@@ -10,25 +10,25 @@ cd /opt/
 sudo mkdir SuperNESPi
 cd SuperNESPi
 
-script=softshutdown.py
+script=retropie_SafeShutdown.py
 
 if [ -e $script ];
 	then
 		echo "Script softshutdown.py already exists. Doing nothing."
 	else
-		wget "https://raw.githubusercontent.com/DeikiChen/softpower/master/softshutdown.py"
+		wget "https://raw.githubusercontent.com/SNESPi/Super-NESPi-Case/master/retropie_SafeShutdown.py"
 fi
 
 #Step 4) Set sutostart ---------------------------
 cd /etc/
 RC=rc.local
 
-if grep -q "sudo python \/opt\/SuperNESPi\/softshutdown.py \&" "$RC";
+if grep -q "sudo python \/opt\/SuperNESPi\/retropie_SafeShutdown.py \&" "$RC";
 
 	then
 		echo "File /etc/rc.local already configured. Doing nothing."
 	else
-		sed -i -e "s/^exit 0/sudo python \/opt\/SuperNESPi\/softshutdown.py \&\n&/g" "$RC"
+		sed -i -e "s/^exit 0/sudo python \/opt\/SuperNESPi\/retropie_SafeShutdown.py \&\n&/g" "$RC"
 		echo "File /etc/rc.local configured."
 fi
 #-----------------------------------------------------------
@@ -39,6 +39,5 @@ echo "SuperNESPi Case installation done. Will now reboot after 3 seconds."
 sleep 3
 sudo reboot
 #-----------------------------------------------------------
-
 
 
